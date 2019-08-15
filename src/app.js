ba8d6801772ed907541c98b32d8304db79306ff2
Neil Berg <neil.berg14@gmail.com>
2019-08-14 20:17:14 -0700

    serving static React router pages again

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('./db/mongoose');
 const userRouter = require('./routers/users');
 const taskRouter = require('./routers/tasks');
@@ -9,5 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+// Serve static files from the React app in task-manager/client/build
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+// Catchall return React index page for not found pages
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+});
 
 module.exports = app;
